615bce2ae68a47a95e0f5c741f08a2da4ae395b2
Peter Palfrader <peter@palfrader.org>
2017-08-02 18:44:49 +0200

    At least on lobos we no longer seem to require running a 10.0.* hp-health package.  Remove the pin everywhere and see how that goes

--- modules/hardware/manifests/init.pp
+++ modules/hardware/manifests/init.pp
@@ -5,10 +5,10 @@ class hardware {
 		file { '/etc/apt/preferences.d/dsa-hp-tools':
 			content => "Package: hp-health\nPin: version 8.6*\nPin-Priority: 1100\n"
 		}
-	} elsif "$::systemproductname" in ["ProLiant DL380 G5", "ProLiant DL360 G5", "ProLiant DL380 G6", "ProLiant DL360 G6", "ProLiant BL495c G5"] {
-		file { '/etc/apt/preferences.d/dsa-hp-tools':
-			content => "Package: hp-health\nPin: version 10.0.*\nPin-Priority: 1100\n"
-		}
+	#} elsif "$::systemproductname" in ["ProLiant DL380 G5", "ProLiant DL360 G5", "ProLiant DL380 G6", "ProLiant DL360 G6", "ProLiant BL495c G5"] {
+	#	file { '/etc/apt/preferences.d/dsa-hp-tools':
+	#		content => "Package: hp-health\nPin: version 10.0.*\nPin-Priority: 1100\n"
+	#	}
 	} else {
 		file { '/etc/apt/preferences.d/dsa-hp-tools':
 			ensure => absent
