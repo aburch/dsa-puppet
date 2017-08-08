6e9c4ff8298e3b2fac751495297a8ac02d2dd8ee
Julien Cristau <jcristau@debian.org>
2017-08-08 19:10:27 -0400

    Fix dell harder
    Their packages need libssl1.0.0 which is only in jessie, and don't
    depend on it.

--- modules/hardware/manifests/raid/dell.pp
+++ modules/hardware/manifests/raid/dell.pp
@@ -1,11 +1,19 @@
 class hardware::raid::dell {
+	site::aptrepo { 'jessie.dell':
+		url        => 'http://deb.debian.org/debian',
+		suite      => 'jessie',
+		components => 'main',
+	}
 	site::aptrepo { 'debian.restricted.dell':
 		url        => 'http://db.debian.org/debian-admin',
 		suite      => 'jessie-restricted',
 		components => 'non-free',
 	}
 
-	package { 'srvadmin-all':
+	package { 'libssl1.0.0':
+		ensure	=> installed,
+	}
+	package { 'srvadmin-storage-cli':
 		ensure  => installed,
 		tag    => extra_repo,
 	}
