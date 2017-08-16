a640453031f7f590851d8e0f27fb3de39be2801a
Peter Palfrader <peter@palfrader.org>
2017-08-16 13:47:35 +0200

    And set net.ifnames to 0

--- modules/grub/manifests/init.pp
+++ modules/grub/manifests/init.pp
@@ -1,7 +1,19 @@
 class grub {
 	if ($::kernel == 'Linux' and $::is_virtual and $::virtual == 'kvm') {
 		file { '/etc/default/grub':
-			content  => template('grub/etc-default-grub.erb'),
+			# restore to default
+			source => 'puppet:///modules/grub/etc-default-grub',
+			notify  => Exec['update-grub']
+		}
+		file { '/etc/default/grub.d':
+			ensure => directory
+		}
+		file { '/etc/default/grub.d/puppet-grub-serial.cfg':
+			content  => template('grub/puppet-grub-serial.cfg.erb'),
+			notify  => Exec['update-grub']
+		}
+		file { '/etc/default/grub.d/puppet-net-ifnames.cfg':
+			content  => template('grub/puppet-net-ifnames.cfg.erb'),
 			notify  => Exec['update-grub']
 		}
 
