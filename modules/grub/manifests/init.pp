ef087f2fe05308f8d399c790762a7745f2c63a09
Peter Palfrader <peter@palfrader.org>
2017-08-18 14:44:47 +0200

    puppetized grub on mirror-skroutz, mirror-accumu

--- modules/grub/manifests/init.pp
+++ modules/grub/manifests/init.pp
@@ -3,7 +3,11 @@ class grub {
 		$grub_manage = true
 		$grub_do_serial = true
 		$grub_do_ifnames = true
-	} elsif $::hostname in [ubc-enc2bl01,ubc-enc2bl02,ubc-enc2bl09,ubc-enc2bl10,casulana,mirror-anu,sallinen,storace] {
+	} elsif $::hostname in [ubc-enc2bl01,ubc-enc2bl02,ubc-enc2bl09,ubc-enc2bl10,casulana,mirror-anu,sallinen,storace,mirror-skroutz] {
+		$grub_manage = true
+		$grub_do_serial = true
+		$grub_do_nopat = true
+	} elsif $::hostname in [mirror-accumu] {
 		$grub_manage = true
 		$grub_do_serial = true
 		$grub_do_nopat = true
