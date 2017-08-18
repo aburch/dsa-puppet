a5356e1286dff8f4ad228a411e2efe3bd6114cc0
Peter Palfrader <peter@palfrader.org>
2017-08-18 14:52:35 +0200

    split grub and kernel serial

--- modules/grub/manifests/init.pp
+++ modules/grub/manifests/init.pp
@@ -1,15 +1,18 @@
 class grub {
 	if ($::kernel == 'Linux' and $::is_virtual and $::virtual == 'kvm') {
 		$grub_manage = true
-		$grub_do_serial = true
+		$grub_do_kernel_serial = true
+		$grub_do_grub_serial = true
 		$grub_do_ifnames = true
 	} elsif $::hostname in [ubc-enc2bl01,ubc-enc2bl02,ubc-enc2bl09,ubc-enc2bl10,casulana,mirror-anu,sallinen,storace,mirror-skroutz] {
 		$grub_manage = true
-		$grub_do_serial = true
+		$grub_do_kernel_serial = true
+		$grub_do_grub_serial = true
 		$grub_do_nopat = true
 	} elsif $::hostname in [mirror-accumu] {
 		$grub_manage = true
-		$grub_do_serial = true
+		$grub_do_kernel_serial = true
+		$grub_do_grub_serial = true
 		$grub_do_nopat = true
 	} else {
 		$grub_manage = false
@@ -28,8 +31,14 @@ class grub {
 		}
 
 		file { '/etc/default/grub.d/puppet-grub-serial.cfg':
-			ensure => $grub_do_serial ? { true  => 'present', default => 'absent' },
+			ensure => $grub_do_kernel_serial ? { true  => 'present', default => 'absent' },
 			content  => template('grub/puppet-grub-serial.cfg.erb'),
+			notify  => Exec['update-grub']
+		}
+
+		file { '/etc/default/grub.d/puppet-kernel-serial.cfg':
+			ensure => $grub_do_grub_serial ? { true  => 'present', default => 'absent' },
+			content  => template('grub/puppet-kernel-serial.cfg.erb'),
 			notify  => Exec['update-grub']
 		}
 
