0564e0c6662530d56ef7276a5ecb53881a218b5a
Peter Palfrader <peter@palfrader.org>
2017-08-28 13:49:37 +0200

    run dsa-check-hpssacli out of cron on smartarraycontroller_hpsa hosts

--- modules/hardware/manifests/raid/proliant.pp
+++ modules/hardware/manifests/raid/proliant.pp
@@ -26,6 +26,19 @@ class hardware::raid::proliant {
 				ensure => installed,
 			}
 		}
+
+		file { '/etc/cron.d/puppet-nagios-hpsa':
+			ensure => ($::smartarraycontroller_hpsa) ? {
+				true    => 'present',
+				default => 'absent'
+			},
+			content  => @(EOF)
+				SHELL=/bin/bash
+				PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/nagios/plugins
+				42 * * * 0 root sleep $(( $RANDOM \% 900 )); dsa-wrap-nagios-check dsa-check-hpssacli
+				| EOF
+
+		}
 	} else {
 		site::aptrepo { 'debian.restricted':
 			ensure => absent,
