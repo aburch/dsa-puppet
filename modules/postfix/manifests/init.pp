68de92dcdd7f2ef714a5ad3a8e5c2a4088b18bd2
Peter Palfrader <peter@palfrader.org>
2017-08-30 08:31:12 +0000

    put a basic postfix config in place

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -18,4 +18,22 @@ class postfix {
 	munin::check { 'postfix_mailvolume': }
 	munin::check { 'ps_smtp': script => 'ps_' }
 	munin::check { 'ps_smtpd': script => 'ps_' }
+
+	if ! has_role('lists') {
+		concat { '/etc/postfix/main.cf':
+			notify  => Exec['service postfix reload'],
+		}
+		concat::fragment { 'puppet-postfix-main.cf--header':
+			target => '/etc/postfix/main.cf',
+			order  => '000',
+			content => template('postfix/main.cf-header.erb')
+		}
+	}
+
+	exec { 'service postfix reload':
+		path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+		command     => 'service postfix reload',
+		refreshonly => true,
+		require =>  Package['postfix'],
+	}
 }
