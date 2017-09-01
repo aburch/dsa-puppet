cb406afb7d640308d5fc9b25fef6f8512d8e24b6
Ansgar Burchardt <ansgar@debian.org>
2017-09-01 21:09:59 +0200

    security upload host: /etc/ssh/userkeys/dak should exist

--- modules/roles/manifests/security_upload.pp
+++ modules/roles/manifests/security_upload.pp
@@ -6,6 +6,13 @@ class roles::security_upload {
 		group	=> debadmin,
 	}
 
+	file { '/etc/ssh/userkeys/dak':
+		ensure  => present,
+		mode    => '644',
+		owner   => dak,
+		group   => debadmin,
+	}
+
 	vsftpd::site { 'security-upload':
 		banner     => 'ftp.security.upload.debian.org FTP server',
 		logfile    => '/var/log/ftp/vsftpd-security.upload.debian.org.log',
